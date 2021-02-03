0e6d58f440b31e204481afdf64af255807ebc211
Edwin Gamboa <edwin.gamboa@ceiba.com.co>
2021-02-03 11:07:13 -0500

    Se cambia el nombre del equipo por Halcones

---
+++ src/test/java/com/serverless/ResponderModelTest.java
@@ -0,0 +1,17 @@
+package com.serverless;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResponderModelTest {
+
+    @Test
+    public void  cuandoPreguntanPorElNombreRespondeHalcones(){
+        ResponderModel responder = new ResponderModel();
+        String pregunta = "d4f58f60: what is your name";
+        String respuesta = responder.answer(pregunta);
+        assertThat(respuesta).isEqualTo("halcones");
+    }
+
+}
